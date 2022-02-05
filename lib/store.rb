6c4b7477188217e50a419cb6d9f0862b3fb118af
RoneyThomas <roneythomas6@gmail.com>
2022-02-05 12:18:08 -0500

    Exercise 1 done

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,10 @@
 class Store < ActiveRecord::Base
+  validates :annual_revenue, numericality: { greater_than: 0 }
+  validate :male_or_female, on: :create
 
+  def male_or_female
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "all stores must carry apparel")
+    end
+  end
 end
